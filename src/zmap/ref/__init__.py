d14a0322fcd02e6c4437bd737361074da1c15a2b
Dan Wagner <38510695+wagnerde@users.noreply.github.com>
2025-11-19 20:01:36 -0800

    edit load_consensus_markers.py fn

--- src/zmap/ref/__init__.py
+++ src/zmap/ref/__init__.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
 
-from .get_consensus_markers import get_consensus_markers
+from .get_consensus_markers import load_consensus_markers
 
-__all__ = ["get_consensus_markers"]
+__all__ = ["load_consensus_markers"]
