95b3ee7da1fd0bbcc7cc4f4f757e614d3e9f3a19
Dan Wagner <38510695+wagnerde@users.noreply.github.com>
2026-03-09 01:27:10 -0700

    clean up public facing api

--- src/zmap/dotplot/__init__.py
+++ src/zmap/dotplot/__init__.py
@@ -1,14 +1,12 @@
 from __future__ import annotations
 
-from .dotplot_gene import gene_groups_vs_time, gene_groups_vs_studies, gene_groups_vs_time_and_studies 
+from .dotplot_gene import gene_groups_vs_time_and_studies 
 from .dotplot_group import group_siblings_vs_markers, group_descendants_vs_markers
 
 gene_view = gene_groups_vs_time_and_studies
 group_view = group_siblings_vs_markers
 
-__all__ = ["gene_groups_vs_time", 
-		   "gene_groups_vs_studies",
-		   "gene_groups_vs_time_and_studies",
+__all__ = ["gene_groups_vs_time_and_studies",
 		   "group_siblings_vs_markers",
 		   "group_descendants_vs_markers",
 		   "gene_view",
--- src/zmap/reference/__init__.py
+++ src/zmap/reference/__init__.py
@@ -1,9 +1,7 @@
 from __future__ import annotations
 
 from .markers import load_consensus_markers
-from .load_h5ad import load_zmap_h5ad, preprocess_tpmlog, download_zmap_h5ad
+from .load_h5ad import load_zmap_h5ad
 
 __all__ = ["load_consensus_markers", 
-		   "load_zmap_h5ad", 
-		   "preprocess_tpmlog", 
-		   "download_zmap_h5ad"]
+		   "load_zmap_h5ad"]
